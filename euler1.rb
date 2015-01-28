4ddbb6695cd372a0b8f7edee4a32d908e09ada4a
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-01-28 14:02:29 +1300

    Ruby Practice

---
+++ euler1.rb
@@ -0,0 +1,15 @@
+puts "Problem 1"
+puts "sum of all multiples of 3 and 5 under hundred"
+
+sum = 0
+number =0
+
+while number <10
+	
+	if number % 3== 0 || number%5 == 0
+		sum = sum + number
+		
+	end
+	number=number+1
+end
+puts "Sum is : #{sum}"
